582ab744fefb5c6312f7cadfa21e2bfea242bbc9
Samhita-Veturi <67774716+Samhita-Veturi@users.noreply.github.com>
2021-12-11 19:02:26 +0530

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,4 +1,8 @@
 Shake_It_Off = "";
+Left_X = "";
+Left_Y = "";
+Right_X = "";
+Right_Y = "";
 function Play(){
     Shake_It_Off.play();
 }
@@ -13,6 +17,24 @@ function setup(){
     Canvas.position(375, 200);
     Video = createCapture(VIDEO);
     Video.hide();
+
+    poseNet = ml5.poseNet(Video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("Model Initialized!")
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        Left_X = results[0].pose.leftWrist.x;
+        Left_Y = results[0].pose.leftWrist.y;
+        console.log("Left wrist X: " + Left_X + " Left wrist Y: " + Left_Y);
+        
+        Right_X = results[0].pose.rightWrist.x;
+        Right_Y = results[0].pose.rightWrist.y;
+        console.log("Right wrist X: " + Right_X + " Right wrist Y: " + Right_Y);
+    }
 }
 function draw(){
     image(Video, 0, 0, 700, 500);
